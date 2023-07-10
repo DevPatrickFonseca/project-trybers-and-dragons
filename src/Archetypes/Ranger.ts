76a95ab4df9b142a88ce3c8265523c04f5f0cd94
PFonsecaFV <pfonsecafv@gmail.com>
2023-07-10 15:21:39 -0300

    :sparkles: Req 05 - Ranger class from Archetype - Implementing Function

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,24 @@
+import Archetype from './Archetype';
+
+import { EnergyType } from '../Energy';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger.instances += 1;
+  }
+  
+  static createdArchetypeInstances(): number {
+    return Ranger.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Ranger;
